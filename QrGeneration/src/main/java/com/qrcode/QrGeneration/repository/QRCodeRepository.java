c0f08aff690a98ef64489a58fe298df1f6a3d7ea
NaveenReddyMatta <139575861+NaveenReddyMatta@users.noreply.github.com>
2025-04-24 12:59:01 +0530

    Delete QrGeneration directory

--- QrGeneration/src/main/java/com/qrcode/QrGeneration/repository/QRCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qrcode.QrGeneration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.qrcode.QrGeneration.model.QRCodeEntity;
-
-public interface QRCodeRepository extends JpaRepository<QRCodeEntity, Long> {
-}
